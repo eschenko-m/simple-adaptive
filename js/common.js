dd92e08cad74680f10777ef4b464369361f29ea5
Mikhail Eschenko <eschenko.mikhail@gmail.com>
2015-07-28 20:54:24 +0400

    add a soc.icons+toplinks in header

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+$(".auth_buttons").click(function(){
+	$(this).next().slideToggle();
+});
+
 	//Таймер обратного отсчета
 	//Документация: http://keith-wood.name/countdown.html
 	//<div class="countdown" date-time="2015-01-07"></div>
